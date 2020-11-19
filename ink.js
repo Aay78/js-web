18e3aa341e4f94b2ba24f8721181c5c984233b2c
Aay78 <71471836+Aay78@users.noreply.github.com>
2020-11-19 15:05:52 -0800

    Add files via upload

---
+++ ink.js
@@ -0,0 +1,13 @@
+var snake = document.createElement('IMG');
+snake.setAttribute('src', 'kyoya.jpg');
+document.body.appendChild(snake);
+
+var places = document.createElement('DIV');
+places.setAttribute('backgroundColor', 'red');
+var word = document.createElement('P');
+var word2 = document.createTextNode("Kyoya Tategami");
+places.appendChild(word2);
+places.appendChild(word);
+document.body.appendChild(places);
+
+
